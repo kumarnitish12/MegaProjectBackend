df2f6ecb3bd9edaff1042de6a93016bee356a09a
Nitish Kumar <nk8970482@gmail.com>
2026-04-05 13:11:43 +0530

    user and video model

--- src/index.js
+++ src/index.js
@@ -7,3 +7,11 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running in port : ${process.env.PORT}`)
+    })
+})
+.catch(err => {
+    console.log("Mongo DB connection failed !!!");
+})
---
+++ src/models/video.models.js
@@ -0,0 +1,36 @@
+import mongoose,{Schema} from "mongoose"
+
+import mongooseAggregatePaginate from
+"mongoose-aggregate-paginate-v2"
+const videoSchema = new Schema ({
+    videoFile:{
+        type:String,//cloudinary url
+        required:true
+    },
+    thumbnail:{
+        type:String, //cloudanary url
+        requird:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+
+},{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video",videoSchema)
